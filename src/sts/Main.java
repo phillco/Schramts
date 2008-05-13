176e516df67390678118ff0112a4133b6e2ed8b0
Phillip Cohen <pc@phillipcohen.net>
2008-05-13 16:42:09 +0000

    Added base for our RTS.

---
+++ src/sts/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sts;
+
+/**
+ *
+ * @author Phillip Cohen
+ */
+public class Main
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+}
